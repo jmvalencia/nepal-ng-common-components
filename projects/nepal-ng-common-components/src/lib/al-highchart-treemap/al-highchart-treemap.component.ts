bb01cc470302a105b66081bd14ca3f47ca5a8e31
Rob Parker <Robert.parker@alertlogic.com>
2019-06-13 12:21:00 +0100

    adjust formatting of tooltip display on treemap areas

--- projects/nepal-ng-common-components/src/lib/al-highchart-treemap/al-highchart-treemap.component.ts
+++ projects/nepal-ng-common-components/src/lib/al-highchart-treemap/al-highchart-treemap.component.ts
@@ -7,70 +7,77 @@ import { Input, Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import * as Highcharts from 'highcharts';
 
 @Component({
-    selector: 'al-highchart-treemap',
-    templateUrl: './al-highchart-treemap.component.html',
-    styleUrls: ['./al-highchart-treemap.component.scss']
+  selector: 'al-highchart-treemap',
+  templateUrl: './al-highchart-treemap.component.html',
+  styleUrls: ['./al-highchart-treemap.component.scss']
 })
 export class AlHighchartTreeMapComponent implements OnInit {
 
-    public map: any;
-    public themeToggle = false;
+  public map: any;
+  public themeToggle = false;
 
-    @ViewChild('treeMap') treeMap: ElementRef;
-    /**
-     * Input to populate the graph - set to 'any' until backend is defined, allowing us to build
-     * an interface
-     */
-    @Input() config: any;
+  @ViewChild('treeMap') treeMap: ElementRef;
+  /**
+   * Input to populate the graph - set to 'any' until backend is defined, allowing us to build
+   * an interface
+   */
+  @Input() config: any;
 
-    ngOnInit() {
-        this.map = Highcharts.chart(this.treeMap.nativeElement, {
-            series: [{
-                type: 'treemap',
-                layoutAlgorithm: 'strip',
-                levels: [{
-                    level: 1,
-                    dataLabels: {
-                        enabled: true,
-                        align: 'left',
-                        verticalAlign: 'top',
-                        style: {
-                            fontSize: '15px',
-                            fontWeight: 'bold',
-                            textOutline: '0px',
-                            color: 'white'
-                        }
-                    }
-                }],
-            data: this.config,
-            }],
-            credits: {
-                enabled: false
-            },
-            title: {
-                text: ''
+  ngOnInit() {
+    this.map = Highcharts.chart(this.treeMap.nativeElement, {
+      series: [{
+        type: 'treemap',
+        layoutAlgorithm: 'strip',
+        levels: [{
+          level: 1,
+          dataLabels: {
+            enabled: true,
+            align: 'left',
+            verticalAlign: 'top',
+            style: {
+              fontSize: '15px',
+              fontWeight: 'bold',
+              textOutline: '0px',
+              color: 'white'
             }
-        });
-    }
+          },
 
-    toggleTheme() {
-        this.themeToggle = !this.themeToggle;
-        this.toggleDarkTheme();
-    }
+        }],
+        tooltip: {
+          // tslint:disable-next-line
+          pointFormatter: function () {
+            return this.value;
+          },
+        },
+        data: this.config,
+      }],
+      credits: {
+        enabled: false
+      },
+      title: {
+        text: ''
+      }
+    });
+  }
 
-    toggleDarkTheme() {
-        if ( this.themeToggle ) {
-            this.map.update({
-                chart: {
-                    backgroundColor: '#3C3C3C',
-                },
-            });
-        } else  {
-            this.map.update({
-                chart: {
-                    backgroundColor: '#ffffff',
-                }
-            });
+  toggleTheme() {
+    this.themeToggle = !this.themeToggle;
+    this.toggleDarkTheme();
+  }
+
+  toggleDarkTheme() {
+    if (this.themeToggle) {
+      this.map.update({
+        chart: {
+          backgroundColor: '#3C3C3C',
+        },
+      });
+    } else {
+      this.map.update({
+        chart: {
+          backgroundColor: '#ffffff',
         }
+      });
     }
+  }
 }
