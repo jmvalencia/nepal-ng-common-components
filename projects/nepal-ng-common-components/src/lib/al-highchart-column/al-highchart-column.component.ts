e580666e716f6110e09bfaa1dc7feb8033c6792c
mcastleton-admin <mcastleton-admin@AL-CAR-09636.local>
2019-07-03 12:54:17 +0100

    removing console.log

--- projects/nepal-ng-common-components/src/lib/al-highchart-column/al-highchart-column.component.ts
+++ projects/nepal-ng-common-components/src/lib/al-highchart-column/al-highchart-column.component.ts
@@ -56,7 +56,6 @@ export class AlHighchartColumnComponent implements OnChanges {
                           },
                           bubbles: true
                         }));
-                        console.log(event)
                       }
                     }
                 }
